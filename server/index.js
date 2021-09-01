b83bd52ea5100a6b9cdad753d9ae9140445f22a8
Cassie Anderson <cbp.smith@gmail.com>
2021-09-01 11:32:59 -0400

    added more to server/web socket files to get up and running but not connected to our app yet

--- server/index.js
+++ server/index.js
@@ -15,6 +15,21 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
   });
 
 server.listen(port, hostname, () => {
