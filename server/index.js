3445c84af9950628e16afa18da58e8d09ba65ac0
unknown <TFurman47@gmail.com>
2021-09-02 21:21:56 -0500

    added connection to database.  Should be able to implement axios POST/GET requests now for future

--- server/index.js
+++ server/index.js
@@ -4,15 +4,29 @@ const port = 3700;
 const express = require('express');
 const app = express();
 const http = require('http');
-
-//Connect to database
-const pgp = require('pg-promise');
-const axios = require('axios');
-const {dirname}
-
-
-
 const server = http.createServer(app);
+
+//Connect to database & save data to json file for front end to use for prop/state management
+const pgp = require('pg-promise')();
+const axios = require('axios');
+const {dirname} = require('path');
+
+//var DATABASE_ID = 'yayzkssa';
+//var DATABASE_PASSWORD = 'Uw9I3snTPe8dTY307iJO_1MTGmZwm_nA';
+//var DATABASE_HOST = 'chunee.db.elephantsql.com';
+//var DATABASE_USER = 'yayzkssa';
+
+ const dbsettings = process.env.DATABASE_URL || pgp({
+   database: 'yayzkssa',
+   password: 'Uw9I3snTPe8dTY307iJO_1MTGmZwm_nA',
+   host: 'chunee.db.elephantsql.com',
+   user: 'yayzkssa'
+ })
+ const db = (dbsettings);
+
+
+
+//socket io
 const { Server } = require("socket.io");
 const io = new Server(server);
 
