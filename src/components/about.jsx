b730be93418a2b9e8ebdff70129e3dff1c0b0530
Cassie Anderson <cbp.smith@gmail.com>
2021-09-25 21:41:40 -0400

    changes for the about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,15 @@ class About extends Component {
     render() {
         return ( 
             <div>
-                <h3>Hey! We're great! Hop in a group run and check us out.</h3>
+                <img src="./runner.jpg"/>
+                <p>Hey! We're passionate about running and hope you'll find our service useful. Join a run and meet other like minded folks. Compete for the fastest
+                    5 miler and then part ways. Keep your running stats in one place. Enjoy your run!</p>
+                <div>Take a look at our GitHub repos: 
+                    <ul>
+                        <li><a href="https://github.com/TaylorFurman/runningroyalbackend">Backend</a></li> 
+                        <li><a href="https://github.com/TaylorFurman/runningroyal">Frontend</a></li>
+                    </ul>
+                </div>
             </div>
         );
     }
